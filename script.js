5defc01646fd8676b1f5d4512216f0babfe97e32
Fred-Bell <fwgbell@gmail.com>
2019-01-03 16:02:11 +0000

    added scroll script to findOutMore

---
+++ script.js
@@ -0,0 +1,5 @@
+const findOutMore = document.getElementsByClassName('find-out-more')[0];
+findOutMore.addEventListener('click', function(){
+  const about = document.getElementById('about');
+  about.scrollIntoView();
+});
