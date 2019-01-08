255b706aed8f1b9e7bb60adeaa29c79e51548a59
Fred-Bell <fwgbell@gmail.com>
2019-01-08 14:54:32 +0000

    improved nav bar and added nav position change

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const findOutMore = document.getElementsByClassName('find-out-more')[0];
+const nav = document.getElementsByTagName('nav')[0];
+const about = document.getElementById('about');
+
 findOutMore.addEventListener('click', function(){
-  const about = document.getElementById('about');
-  about.scrollIntoView();
+  nav.scrollIntoView();
+});
+
+const aboutHeight = about.scrollHeight;
+
+window.addEventListener('scroll', function(){
+  const scrollAmount = window.scrollY;
+  if(scrollAmount > aboutHeight){
+    nav.style.position = 'fixed';
+  } else {
+    nav.style.position = 'static';
+  }
 });
